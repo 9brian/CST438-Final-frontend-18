f8e19cb2df543c76fc44094ab51a42365c10317c
Jbaires831 <jbaires616@gmail.com>
2023-12-06 14:25:56 -0800

    set up for home page and flight display page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
-import ListAssignment from './components/ListAssignment';
-import GradeAssignment from './components/GradeAssignment';
+import React from 'react';
+import HomePage from './HomePage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h2>Gradebook</h2>
-      <BrowserRouter>
-          <div>
-            <Switch>
-              <Route exact path="/" component={ListAssignment} />
-              <Route path="/gradeAssignment" component={GradeAssignment} />
-              <Route render={ () => <h1>Page not found</h1>} />
-            </Switch>
-          </div>
-        </BrowserRouter>
+    <div>
+      <HomePage></HomePage>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+const HomePage = () => {
+    const [depatureCode, setDepartureCode] = useState('');
+    const [arrivalCode, setArrivalCode] = useState('');
+    const [depatureDate, setDepartureDate] = useState('');
+    const [numAdults, setNumAdults] = useState('1');
+    const [serachResult, setSearchResults] = useState(null);
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        //handle  search based on criteria 
+        console.log("search criteria: ", { depatureCode, arrivalCode, depatureDate, numAdults });
+        //add api request logic
+
+        const apiUrl = `/apiflights/${departureCode}/${arrivalCode}/${departureDate}/${numAdults}/2`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            setSearchResults(data);
+        } catch (error) {
+            console.log("error fetching data: ", error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Flight search</h1>
+            <form onSubmit={handleSearch}>
+                <label htmlFor='departureCode'>Departure Airport Code:</label>
+                <input
+                    type='text'
+                    id='departureCode'
+                    placeholder='LAX'
+                    value={depatureCode}
+                    onChange={(e) => setDepartureCode(e.target.value)}>Required</input>
+
+
+                <label htmlFor='arrivalCode'>Arrival Airport Code: </label>
+                <input
+                    type='text'
+                    id='arrivalCode'
+                    name='arrivalCode'
+                    placeholder='JFK'
+                    value={arrivalCode}
+                    onChange={(e) => setArrivalCode(e.target.value)}>Required</input>
+
+                <label htmlFor='departureDate'> Departure Date: </label>
+                <input
+                    type='date'
+                    id='departureDate'
+                    name='departureDate'
+                    value={depatureDate}
+                    onChange={(e) => setDepartureDate(e.target.value)}>Required</input>
+
+                <label htmlFor="numAdults">Number of Adults:</label>
+                <select
+                    id="numAdults"
+                    name="numAdults"
+                    value={numAdults}
+                    onChange={(e) => setNumAdults(e.target.value)}
+                    required
+                >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    {/* Add more options as needed */}
+                </select>
+
+                <button type="submit">Search Flights</button>
+            </form>
+
+            {searchResults && (
+                <div>
+                    <h2>Search Results</h2>
+                    {/* Display search results as needed */}
+                    {/* You can map through the results and render them */}
+                    {searchResults.map((result) => (
+                        <div key={result.id}>
+                            {/* Render flight details */}
+                            <p>Flight ID: {result.id}</p>
+                            <p>Carrier: {result.carrier_name}</p>
+                            {/* Add more details as needed */}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+
+    )
+}
